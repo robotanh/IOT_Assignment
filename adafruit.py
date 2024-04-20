1f419d12452098fff26104b57b4f3570a2ce8d34
robotanh <65024155+robotanh@users.noreply.github.com>
2024-04-20 21:30:54 +0700

    Update adafruit.py

--- adafruit.py
+++ adafruit.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 import random
-
+import json
 from Adafruit_IO import MQTTClient
 
 #from simple_ai import image_detector
@@ -17,58 +17,42 @@ from rs485 import *
 #from adafruit import *
 
 class Adafruit_MQTT:
-
-    AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
-
+    AIO_FEED_IDs = ["cambien1", "cambien2"]
     AIO_USERNAME = "robotanh"
-
     AIO_KEY = ""
     
+    # AIO_FEED_IDs = ["assignment.mixer1", "assignment.mixer2"]
+    # AIO_USERNAME = "huytran1305"
+    # AIO_KEY = ""
     recvCallBack = None
 
-
     def connected(self, client):
-
         print("Connected ...")
-
         for feed in self.AIO_FEED_IDs:
-
             client.subscribe(feed)
 
-
     def subscribe(self, client, userdata, mid, granted_qos):
-
-        print("Subscribeb...")
-
+        print("Subscribed...")
 
     def disconnected(self, client):
-
         print("Disconnected...")
-
         sys.exit(1)
 
+    def message(self, client, feed_id, payload):
+        if self.recvCallBack:
+            data_dict = json.loads(payload)
+            self.recvCallBack(feed_id, data_dict)
 
-    def message(self, client , feed_id , payload):
-        
-        self.recvCallBack(feed_id, payload)
-    
-    def setRecvCallBack(self,func):
+    def setRecvCallBack(self, func):
         self.recvCallBack = func
 
     def __init__(self):
-
         self.client = MQTTClient(self.AIO_USERNAME, self.AIO_KEY)
-
         self.client.on_connect = self.connected
-
         self.client.on_disconnect = self.disconnected
-
         self.client.on_message = self.message
-
         self.client.on_subscribe = self.subscribe
-
         self.client.connect()
-
         self.client.loop_background()
 
 
@@ -79,9 +63,10 @@ class Adafruit_MQTT:
 
 # Create an instance of Adafruit_MQTT
 
-mqtt_instance = Adafruit_MQTT()
+# mqtt_instance = Adafruit_MQTT()
 
 
-while True:
-	# readSerial(mqtt_instance.client)
-	time.sleep(2)
+# while True:
+# 	# readSerial(mqtt_instance.client)
+# 	time.sleep(2)
+
