97334dd68fddcc1df7b077ee04bf627e397c77d7
robotanh <robotanh15@gmail.com>
2024-04-22 11:49:20 +0700

    update

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 import random
 import json
 
-mixer1 = ""
-mixer2 = ""
-mixer3 = ""
-nextcycle = ""
-area = ""
-pumpin = ""
-pumpout = ""
+mixer1 = None
+mixer2 = None
+mixer3 = None
+nextcycle = None
+area = None
+pumpin = None
+pumpout = None
+active = False
 
 """
     [
@@ -27,19 +28,27 @@ pumpout = ""
 
 """
 sched = {
-    "cambien1": mixer1,
-    "cambien2": mixer2,
+    "assignment.nextcycle": nextcycle,
     "assignment.mixer1": mixer1,
     "assignment.mixer2": mixer2,
-    "assignment.mixer3": mixer1,
+    "assignment.mixer3": mixer3,
+    "assignment.area": area,
+    "assignment.pumpin": pumpin,
+    "assignment.pumpout": pumpout,
+    "assignment.active": active,
+
 
 }
+sched_active = {}
 
 def data_callback(feed_id, payload):
     print(f"Received data from {feed_id}: {payload}")
     if feed_id in sched:
         # Append the payload to the corresponding list
         sched[feed_id] = payload
+        if sched["assignment.active"] == True:
+            sched_active = sched
+            sched["assignment.active"] = False
     else:
         print("No handler found for feed:", feed_id)
 
